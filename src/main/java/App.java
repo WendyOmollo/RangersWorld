b445101cf35738ba23d76f9df4dd239ac69b34c5
WendyOmollo <omollow280@gmail.com>
2019-09-16 01:08:34 +0300

    Add a layout file for the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,35 @@
+import dao.Sql2oAnimalDao;
+
+import models.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+
 public class App {
-}
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/Animal.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oAnimalDao animalDao = new Sql2oAnimalDao(sql2o);
+        Map<String, Object> model = new HashMap<>();
+
+        get("/",(request, response) -> {
+            return new ModelAndView(model,"layout.hbs");
+        },new HandlebarsTemplateEngine());
+
+
+        }
+
+
+    }
