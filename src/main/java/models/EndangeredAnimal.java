03c6b0826f275b5187a83cb6fbf5485ec9726404
WendyOmollo <omollow280@gmail.com>
2019-09-17 08:04:55 +0300

    Add a test that confirms the instance of an endangered animal

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,11 +1,88 @@
 package models;
 
+import java.util.Objects;
+
 public class EndangeredAnimal extends Animal{
-        private int id;
+    private String type;
+    private int id;
         private String name;
+        private String health;
+        private String age;
+        public static final String ANIMAL_TYPE = "Endangered";
 
 
-    public EndangeredAnimal(int id, String name) {
+
+    public EndangeredAnimal(int id, String name,String health,String age) {
         super(id, name);
+        this.id =id;
+        this.name = name;
+        this.health = health;
+        this.age = age;
+        this.type = ANIMAL_TYPE;
+
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return getId() == that.getId() &&
+                Objects.equals(getType(), that.getType()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getHealth(), that.getHealth()) &&
+                Objects.equals(getAge(), that.getAge());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getType(), getId(), getName(), getHealth(), getAge());
     }
 }
--- src/test/java/models/EndangeredAnimalTest.java
+++ src/test/java/models/EndangeredAnimalTest.java
@@ -2,6 +2,8 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +15,17 @@ public class EndangeredAnimalTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Rule
+    public  DatabaseRule database = new DatabaseRule();
+
+    public EndangeredAnimal setUpNewEndangered(){
+        return new EndangeredAnimal(1,"Rhino","Okay","young");
+    }
+
+    @Test
+    public void addEndangeredAnimal_true(){
+        EndangeredAnimal weak = setUpNewEndangered();
+        assertTrue(weak instanceof EndangeredAnimal);
     }
 }
